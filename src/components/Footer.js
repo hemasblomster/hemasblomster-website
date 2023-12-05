0a42103be1291ed15c911160bf42c425c6e98ee8
Ramadan Cilomba <rcilomba@yahoo.com>
2023-12-06 00:34:55 +0100

    import changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FaInstagram, FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
--- src/components/MenuFlower.js
+++ src/components/MenuFlower.js
@@ -1,5 +1,5 @@
 import logo from "../images/logo.png";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MenuFlower = () => {
   return (
